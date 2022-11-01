8a33ef4e479260a355f6ad618f0df49b77f86999
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2022-11-01 12:43:45 +0100

    test license
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- org.gecko.emf.util.model/src/org/gecko/emf/util/helper/UtilModelHelper.java
+++ org.gecko.emf.util.model/src/org/gecko/emf/util/helper/UtilModelHelper.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2012 - 2022 Data In Motion and others.
+ * Copyright (c) 2012 - 2022 Data In Motion Consulting GmbH.
  * All rights reserved. 
  * 
- * This program and the accompanying materials are made available under the terms of the 
+ * This program and the accompanying materials are made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  * 
