7e43d05dbba354d8d55e1abf896ef912627a660a
Ilenia Salvadori <i.salvadori@data-in-motion.biz>
2023-01-10 13:48:43 +0100

    changed javax with jakarta namespace
    Signed-off-by: Ilenia Salvadori <i.salvadori@data-in-motion.biz>

--- org.gecko.emf.rest/src/org/gecko/emf/jaxrs/EMFResourceMessageBodyHandler.java
+++ org.gecko.emf.rest/src/org/gecko/emf/jaxrs/EMFResourceMessageBodyHandler.java
@@ -17,14 +17,14 @@ import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.ext.MessageBodyReader;
-import javax.ws.rs.ext.MessageBodyWriter;
-import javax.ws.rs.ext.Provider;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.MessageBodyReader;
+import jakarta.ws.rs.ext.MessageBodyWriter;
+import jakarta.ws.rs.ext.Provider;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -69,7 +69,7 @@ public class EMFResourceMessageBodyHandler<R extends Resource, W extends Resourc
 	
 	/*
 	 * (non-Javadoc)
-	 * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
+	 * @see jakarta.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], jakarta.ws.rs.core.MediaType)
 	 */
 	@Override
 	public boolean isWriteable(Class<?> type, Type genericType,
@@ -79,7 +79,7 @@ public class EMFResourceMessageBodyHandler<R extends Resource, W extends Resourc
 
 	/*
 	 * (non-Javadoc)
-	 * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
+	 * @see jakarta.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], jakarta.ws.rs.core.MediaType)
 	 */
 	@Override
 	public long getSize(W t, Class<?> type, Type genericType,
@@ -89,7 +89,7 @@ public class EMFResourceMessageBodyHandler<R extends Resource, W extends Resourc
 
 	/*
 	 * (non-Javadoc)
-	 * @see javax.ws.rs.ext.MessageBodyReader#isReadable(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
+	 * @see jakarta.ws.rs.ext.MessageBodyReader#isReadable(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], jakarta.ws.rs.core.MediaType)
 	 */
 	@Override
 	public boolean isReadable(Class<?> type, Type genericType,
@@ -111,7 +111,7 @@ public class EMFResourceMessageBodyHandler<R extends Resource, W extends Resourc
 	}
 
 	/* (non-Javadoc)
-	 * @see javax.ws.rs.ext.MessageBodyReader#readFrom(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap, java.io.InputStream)
+	 * @see jakarta.ws.rs.ext.MessageBodyReader#readFrom(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], jakarta.ws.rs.core.MediaType, jakarta.ws.rs.core.MultivaluedMap, java.io.InputStream)
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
@@ -120,7 +120,7 @@ public class EMFResourceMessageBodyHandler<R extends Resource, W extends Resourc
 	}
 
 	/* (non-Javadoc)
-	 * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
+	 * @see jakarta.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], jakarta.ws.rs.core.MediaType, jakarta.ws.rs.core.MultivaluedMap, java.io.OutputStream)
 	 */
 	@Override
 	public void writeTo(W t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
