0b6fe3a9b0f2505328284bc5ba4c940b839a1dee
Michael H. Siemaszko <m.siemaszko@datainmotion.com>
2023-11-07 21:42:17 +0100

    Jakarta-Javax Split
    Signed-off-by: Michael H. Siemaszko <m.siemaszko@datainmotion.com>

---
+++ org.gecko.emf.rest/src/org/gecko/emf/rest/common/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2012 - 2023 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
+ */
+@org.osgi.annotation.bundle.Export
+package org.gecko.emf.rest.common;
